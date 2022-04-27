3e326f639b2d426bf122281f11f75e8097c900aa
luoziyi <luoziyi1991@163.com>
2022-04-27 11:27:23 +0800

    第一个错误版本

--- 2022/04/Apr27.py
+++ 2022/04/Apr27.py
@@ -27,3 +27,26 @@ def search_01(nums, x):
         else:
             right = mid - 1
     return -1
+
+
+# 二分查找002
+"""
+第一个错误版本；
+假设你有 n 个版本 [1, 2, ..., n]，你想找出导致出错的第一个错误的版本。你可以通过调用isBadVersion(version)判断是否出错
+"""
+
+
+def isBadVersion(x):
+    # 判断是否是错误版本
+    return 0
+
+
+def first_bad_version(n):
+    left, right = 0, n - 1
+    while left <= right:
+        mid = (left + right) >> 1
+        if isBadVersion(mid):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
