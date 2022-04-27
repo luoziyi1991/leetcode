570e4d9a04a87f890e21f8ff9bf504c48a1e274e
luoziyi <luoziyi1991@163.com>
2022-04-27 13:25:09 +0800

    二分搜索并插入

--- 2022/04/Apr27.py
+++ 2022/04/Apr27.py
@@ -50,3 +50,22 @@ def first_bad_version(n):
         else:
             left = mid + 1
     return left
+
+
+# 二分查找003
+"""
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+"""
+
+
+def search_and_insert(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) >> 1
+        if target == nums[mid]:
+            return mid
+        elif target > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
